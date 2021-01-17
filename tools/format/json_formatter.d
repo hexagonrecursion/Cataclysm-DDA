b61446af4ae56fd10624083c004e78c68174f0d2
Andrey Bienkowski <hexagonrecursion@gmail.com>
2021-01-17 07:35:56 +0300

    XXX: amend me

---
+++ tools/format/json_formatter.d
@@ -0,0 +1,59 @@
+tools/format/json_formatter.cgi: src/json.cpp src/json.h src/colony.h \
+ src/cata_assert.h src/enum_conversions.h src/debug.h \
+ src/string_formatter.h src/compatibility.h src/optional.h \
+ src/enum_traits.h src/memory_fast.h src/string_id.h src/cached_options.h \
+ src/cata_utility.h src/output.h src/catacharset.h src/color.h \
+ src/translations.h src/value_ptr.h src/enums.h src/item.h src/calendar.h \
+ src/craft_command.h src/point.h src/recipe.h src/build_reqs.h \
+ src/type_id.h src/int_id.h src/requirements.h src/crafting.h \
+ src/flat_set.h src/gun_mode.h src/io_tags.h src/item_contents.h \
+ src/item_pocket.h src/ret_val.h src/units.h src/math_defines.h \
+ src/units_fwd.h src/visitable.h src/iteminfo_query.h src/item_location.h \
+ src/map_selector.h src/material.h src/fire.h src/safe_reference.h \
+ src/line.h
+src/json.h:
+src/colony.h:
+src/cata_assert.h:
+src/enum_conversions.h:
+src/debug.h:
+src/string_formatter.h:
+src/compatibility.h:
+src/optional.h:
+src/enum_traits.h:
+src/memory_fast.h:
+src/string_id.h:
+src/cached_options.h:
+src/cata_utility.h:
+src/output.h:
+src/catacharset.h:
+src/color.h:
+src/translations.h:
+src/value_ptr.h:
+src/enums.h:
+src/item.h:
+src/calendar.h:
+src/craft_command.h:
+src/point.h:
+src/recipe.h:
+src/build_reqs.h:
+src/type_id.h:
+src/int_id.h:
+src/requirements.h:
+src/crafting.h:
+src/flat_set.h:
+src/gun_mode.h:
+src/io_tags.h:
+src/item_contents.h:
+src/item_pocket.h:
+src/ret_val.h:
+src/units.h:
+src/math_defines.h:
+src/units_fwd.h:
+src/visitable.h:
+src/iteminfo_query.h:
+src/item_location.h:
+src/map_selector.h:
+src/material.h:
+src/fire.h:
+src/safe_reference.h:
+src/line.h:
